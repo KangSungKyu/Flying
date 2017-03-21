4c1262c83d3b97ed9900d241b3a8ad8c1ca16a84
KangSungKyu <tjdrb70@gmail.com>
2017-03-21 21:59:27 +0900

    apk/exe/etc..

---
+++ Assets/Editor/GitMan.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using System.IO;
+
+public class GitMan : ScriptableWizard {
+
+    public string bashPath = "C:/Program Files/Git/git-bash.exe";
+    public string local = "";
+    public string commitmsg = "";
+
+
+    [MenuItem("Edit/Git Commit in Local")]
+    static void GitCommitLocal()
+    {
+        ScriptableWizard.DisplayWizard("Commit in Local", typeof(GitMan), "Commit");
+    }
+    [MenuItem("Edit/Git Push to Hub")]
+    static void GitPushHub()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+    private void OnWizardCreate()
+    {
+        if (Selection.objects == null)
+            return;
+
+        string commit = "git commit -m \"" + commitmsg + "\"";
+
+        Process bash = Process.Start(bashPath);
+
+        bash.StandardInput.Write(commit);
+    }
+
+}
